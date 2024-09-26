64f510b6627f584376f63f2e2e0b01cc8ba2efe9
José Manuel <josemanuel3606@gmail.com>
2024-09-26 21:27:30 +0200

    HTML terminado (CSS y JS inacabados)

---
+++ js/index.js
@@ -0,0 +1,18 @@
+// Selecciona todos los elementos <li> dentro del <ol>
+const items = document.querySelectorAll('ol li, ul li');
+
+// Itera sobre cada <li>
+items.forEach(item => {
+    // Divide el contenido del <li> en dos partes, antes y después de los ":"
+    const parts = item.innerHTML.split(':');
+
+    // Si el símbolo ":" está presente en el <li>
+    if (parts.length > 1) {
+        // La primera parte (antes de ":") la envuelve en <span class="negrita">
+        const boldPart = '<span class="negrita">' + parts[0] + ':</span>';
+
+        // Une la parte en negrita con el resto del contenido
+        const restOfText = parts.slice(1).join(':'); // El resto del texto después de ":"
+        item.innerHTML = boldPart + restOfText;
+    }
+});
